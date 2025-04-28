88f2c02c887b49b30a1cc085d103bb42c44c423e
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-28 14:09:39 +0300

    add jacoco implementation for test coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    jacoco
 }
 
 group = "org.moscow"
@@ -29,4 +30,65 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(19)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        csv.required.set(true)  // Enable CSV reports for additional processing if needed
+        xml.required.set(true)  // Required for coverage-diff to work
+        html.required.set(true) // Human-readable reports
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.test)
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+        }
+    )
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(fileTree(layout.buildDirectory).include("jacoco/test.exec"))
+
+    violationRules {
+        rule {
+            limit {
+                minimum = "1".toBigDecimal() // 100% coverage requirement
+            }
+        }
+        rule {
+            element = "CLASS"
+            includes = listOf("org.moscow.*") // Adjust package name as needed
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "1".toBigDecimal()
+            }
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "1".toBigDecimal()
+            }
+            limit {
+                counter = "METHOD"
+                value = "COVEREDRATIO"
+                minimum = "1".toBigDecimal()
+            }
+        }
+    }
+}
+
+
+tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+    dependsOn(tasks.test)
+
+    classDirectories.setFrom(
+        fileTree("build/classes/kotlin/main") {
+            exclude("**/generated/**")
+        }
+    )
+    sourceDirectories.setFrom(files("src/main/kotlin"))
+    executionData.setFrom(fileTree(layout.buildDirectory).include("jacoco/test.exec"))
 }
