ccafd88b2ccb65d3eb77c8cb8dfaa3d5819c3c45
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-29 08:26:40 +0300

    add parameters to methods

--- src/main/kotlin/logic/usecases/TasksUseCase.kt
+++ src/main/kotlin/logic/usecases/TasksUseCase.kt
@@ -1,14 +1,14 @@
 package logic.usecases
 
+
 import logic.models.Task
 import logic.repositoies.TasksRepository
 
 class TasksUseCase(
     private val tasksRepository: TasksRepository
-
 ) {
-    fun addTask(): Boolean = false
-    fun editTasks(): Boolean = false
+    fun addTask(task: Task): Boolean = false
+    fun editTasks(id: Int?): Boolean = false
     fun deleteTask(): Boolean = false
     fun deleteAllTasks(): Boolean = false
     fun changeTaskState(): Boolean = false
