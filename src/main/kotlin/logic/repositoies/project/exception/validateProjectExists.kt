81b1bb02ef4be4ca931293f7df4958f2dc521851
alaafawzy <alaaf9443@gmail.com>
2025-04-29 19:01:32 +0300

    add function for  validate if  project is existing

---
+++ src/main/kotlin/logic/repositoies/project/exception/validateProjectExists.kt
@@ -0,0 +1,13 @@
+package logic.repositoies.project.exception
+
+import data.repositories.ProjectsRepositoryImpl
+import logic.repositoies.project.ProjectsRepository
+import logic.repositoies.project.exception.ProjectException.ProjectNotFoundException
+import java.util.*
+
+private val projectsRepository: ProjectsRepository = ProjectsRepositoryImpl()
+
+ fun validateProjectExists(projectId: UUID) {
+        projectsRepository.getAllProjects()
+            ?: throw ProjectNotFoundException("Project with $projectId  was not found.")
+    }
