87d0d8c8677e802519d22818803f4a737033e945
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-29 09:57:05 +0300

    add skeleton for validate and hash functions

---
+++ src/main/kotlin/utilities/PasswordExtensions.kt
@@ -0,0 +1,13 @@
+package utilities
+
+fun String.toMD5Hash(): String {
+    return ""
+}
+
+fun String.isValidPasswordFormat(): Boolean {
+    return false
+}
+
+fun String.matchesMD5Hash(expected: String): Boolean {
+    return false
+}
