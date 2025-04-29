d949555c3ef307341d28f0c24dc144c936669da5
alaafawzy <alaaf9443@gmail.com>
2025-04-29 18:54:26 +0300

    add audit-project-repository interface

---
+++ src/main/kotlin/logic/repositoies/project/AuditProjectRepository.kt
@@ -0,0 +1,10 @@
+package logic.repositoies.project
+
+import logic.models.AuditLog
+import java.util.UUID
+
+interface AuditProjectRepository {
+    fun getSpecificAuditByProject( projectId: UUID,auditId: UUID): AuditLog
+    fun getAllAuditByProject( projectId: UUID): List<AuditLog>
+    fun addAuditToProject(projectId: UUID, audit: AuditLog)
+}
