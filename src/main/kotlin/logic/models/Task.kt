c924acc668ea718341230c03d9dd874b1464d4a3
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-29 08:25:23 +0300

    refactor Task Model

--- src/main/kotlin/logic/models/Task.kt
+++ src/main/kotlin/logic/models/Task.kt
@@ -3,10 +3,10 @@ package logic.models
 import java.util.*
 
 data class Task(
-    val id: UUID=UUID.randomUUID(),
-    val title: String,
-    val description: String="",
-    val projectId: UUID=UUID.randomUUID(),
-    val stateId: Int,
-    val state:String,
+    val id: Int? = null,
+    val title: String? = null,
+    val description: String? = "",
+    val projectId: UUID = UUID.randomUUID(),
+    val stateId: Int? = null,
+    val state: String? = null,
 )
