897db52dae0ac8cc53c56b5ee3c79f7b7b0cbbfa
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-04-29 07:29:44 +0300

    add init functions to use case

--- src/main/kotlin/logic/usecases/TasksUseCase.kt
+++ src/main/kotlin/logic/usecases/TasksUseCase.kt
@@ -1,9 +1,16 @@
 package logic.usecases
 
+import logic.models.Task
 import logic.repositoies.TasksRepository
 
 class TasksUseCase(
     private val tasksRepository: TasksRepository
-) {
 
+) {
+    fun addTask(): Boolean = false
+    fun editTasks(): Boolean = false
+    fun deleteTask(): Boolean = false
+    fun deleteAllTasks(): Boolean = false
+    fun changeTaskState(): Boolean = false
+    fun getTaskById(): Boolean = false
 }
