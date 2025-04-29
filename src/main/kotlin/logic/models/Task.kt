f6f36d90d86be12bf1755a6c20054461644bbe41
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-04-29 07:30:04 +0300

    add some attributes in model

--- src/main/kotlin/logic/models/Task.kt
+++ src/main/kotlin/logic/models/Task.kt
@@ -3,9 +3,10 @@ package logic.models
 import java.util.*
 
 data class Task(
-    val id: UUID,
+    val id: UUID=UUID.randomUUID(),
     val title: String,
-    val description: String,
-    val projectId: UUID,
-    val stateId: UUID,
+    val description: String="",
+    val projectId: UUID=UUID.randomUUID(),
+    val stateId: Int,
+    val state:String,
 )
