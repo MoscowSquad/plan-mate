768dcf5de0e3ab41b7a2e6df5658f9d325234534
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-29 10:53:21 +0300

    add implementation for password extensions

--- src/main/kotlin/utilities/PasswordExtensions.kt
+++ src/main/kotlin/utilities/PasswordExtensions.kt
@@ -1,13 +1,18 @@
 package utilities
 
+import java.security.MessageDigest
+
+@OptIn(ExperimentalStdlibApi::class)
 fun String.toMD5Hash(): String {
-    return ""
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(this.toByteArray())
+    return digest.toHexString()
 }
 
 fun String.isValidPasswordFormat(): Boolean {
-    return false
+    val passwordRegex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\w\\s]).{8,}$")
+    return passwordRegex.matches(this)
 }
 
-fun String.matchesMD5Hash(expected: String): Boolean {
-    return false
-}
+fun String.matchesMD5Hash(expected: String): Boolean =
+    this == expected
