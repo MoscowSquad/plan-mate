ef6e42d83f23bfec9efabf95bffd42ff914b641c
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-30 18:01:28 +0300

    implement createTaskHelper

--- src/test/kotlin/fake/createTaskHelper.kt
+++ src/test/kotlin/fake/createTaskHelper.kt
@@ -1,7 +1,21 @@
 package fake
 
+import logic.models.Project
+import logic.models.State
+import logic.models.Task
+
 fun createTaskHelper(
-
-) {
-
+    taskID: Int? = null,
+    taskTitle: String? = null,
+    taskDescription: String? = "",
+    project: Project? = null,
+    state: State? = null
+): Task {
+    return Task(
+        taskID = taskID,
+        taskTitle = taskTitle,
+        taskDescription = taskDescription,
+        project = project,
+        state = state,
+    )
 }
