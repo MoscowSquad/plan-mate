1c64b23d2baebe533ee7ce419091b4890258ffb7
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-30 22:27:16 +0300

    add in ProjectsRepository 1- deleteState 2- getStateById 3- getStatesById

--- src/main/kotlin/data/repositories/ProjectsRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/ProjectsRepositoryImpl.kt
@@ -1,5 +1,19 @@
 package data.repositories
 
+import logic.models.State
 import logic.repositoies.ProjectsRepository
+import java.util.*
 
-class ProjectsRepositoryImpl: ProjectsRepository
+class ProjectsRepositoryImpl: ProjectsRepository {
+    override fun deleteState(projectId: UUID, state: UUID): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getStateById(stateId: UUID): State {
+        TODO("Not yet implemented")
+    }
+
+    override fun getStatesById(projectId: UUID): List<State> {
+        TODO("Not yet implemented")
+    }
+}
--- src/main/kotlin/logic/repositoies/ProjectsRepository.kt
+++ src/main/kotlin/logic/repositoies/ProjectsRepository.kt
@@ -1,5 +1,13 @@
 package logic.repositoies
 
+import logic.models.State
+import java.util.UUID
+
 interface ProjectsRepository {
 
+    fun deleteState(projectId: UUID, state: UUID): Boolean
+
+    fun getStateById(stateId: UUID): State
+
+    fun getStatesById(projectId: UUID): List<State>
 }
