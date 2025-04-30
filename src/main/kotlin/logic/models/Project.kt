3a111841390400c8dcd294ba464d7a324faa2b40
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-04-30 20:50:47 +0300

    refactor: -rename projectID to id           -remove val states: MutableList<State>,           -remove

--- src/main/kotlin/logic/models/Project.kt
+++ src/main/kotlin/logic/models/Project.kt
@@ -3,8 +3,6 @@ package logic.models
 import java.util.*
 
 data class Project(
-    val projectId: UUID,
+    val id: UUID,
     val name: String,
-    val states: MutableList<State>,
-    val tasks: MutableList<Task>,
 )
