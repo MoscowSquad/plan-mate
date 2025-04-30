d399698d190806b3658c35afb95453306d7de2b4
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-04-30 16:32:38 +0300

    refactor model attributes

--- src/main/kotlin/logic/models/Task.kt
+++ src/main/kotlin/logic/models/Task.kt
@@ -1,12 +1,10 @@
 package logic.models
 
-import java.util.*
 
 data class Task(
-    val id: Int? = null,
-    val title: String? = null,
-    val description: String? = "",
-    val projectId: UUID = UUID.randomUUID(),
-    val stateId: Int? = null,
-    val state: String? = null,
+    val taskID: Int? = null,
+    var taskTitle: String? = null,
+    var taskDescription: String? = "",
+    val project: Project?=null,
+    var state: State? = null,
 )
