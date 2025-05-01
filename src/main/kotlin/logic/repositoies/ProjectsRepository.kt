a6ea65247cb9e5e521dedde944744217ff999b4f
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-05-01 15:01:02 +0300

    add save, findById, Delete and AssignUserToProject methods to interface ProjectsRepository.kt

--- src/main/kotlin/logic/repositoies/ProjectsRepository.kt
+++ src/main/kotlin/logic/repositoies/ProjectsRepository.kt
@@ -4,7 +4,8 @@ import logic.models.Project
 import java.util.UUID
 
 interface ProjectsRepository {
-    fun getProjectById(projectId: UUID): Project
-    fun saveProject(project: Project)
-    fun deleteProject(projectId: UUID)
+    fun save(project: Project): Project
+    fun findById(id: UUID): Project?
+    fun delete(id: UUID): Boolean
+    fun assignUserToProject(projectId: UUID, userId: UUID): Boolean
 }
