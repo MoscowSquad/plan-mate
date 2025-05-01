051c7b51e73347711e7b93e7a47407bb35c05fe1
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-01 16:38:57 +0300

    refactor Exception to meaningful Exception class for EditTaskUseCase and check ID is null

--- src/main/kotlin/logic/usecases/tasksUseCases/EditTaskUseCase.kt
+++ src/main/kotlin/logic/usecases/tasksUseCases/EditTaskUseCase.kt
@@ -2,16 +2,18 @@ package logic.usecases.tasksUseCases
 
 import logic.models.Task
 import logic.repositoies.TasksRepository
+import utilities.PropertyNullException
+import utilities.TaskIsNotFoundException
 
 class EditTaskUseCase(
     private val tasksRepository: TasksRepository
 ) {
     fun editTask(task: Task): Task {
         val allTasks = tasksRepository.getAll().toMutableList()
-        val taskID = task.id
-        val taskTitle = task.title ?: throw Exception()
+        val taskID = task.id?: throw PropertyNullException()
+        val taskTitle = task.title ?: throw PropertyNullException()
         val newTask = allTasks
-            .find { currentTask -> currentTask.id == taskID } ?: throw Exception()
+            .find { currentTask -> currentTask.id == taskID } ?: throw TaskIsNotFoundException(taskID)
 
         newTask.title = taskTitle
         newTask.state = task.state
