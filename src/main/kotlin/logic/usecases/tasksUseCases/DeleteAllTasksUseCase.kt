c94eaaebdf6ffdf3ca844714e3befbb2e4e7862e
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-01 15:47:02 +0300

    separate delete all tasks  into DeleteAllTasksUseCase class

---
+++ src/main/kotlin/logic/usecases/tasksUseCases/DeleteAllTasksUseCase.kt
@@ -0,0 +1,17 @@
+package logic.usecases.tasksUseCases
+
+import logic.repositoies.TasksRepository
+
+class DeleteAllTasksUseCase(
+    private val tasksRepository: TasksRepository
+) {
+
+    fun deleteAllTasks(isAdmin: Boolean): Boolean {
+        val allTasks = tasksRepository.getAll().toMutableList()
+        if (isAdmin && allTasks.isNotEmpty()) {
+            allTasks.clear()
+            return true
+        }
+        return throw Exception()
+    }
+}
