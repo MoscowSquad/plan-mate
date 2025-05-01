2ae052ded510c1a85d10a45ba12e5e452697e2be
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-05-01 15:04:56 +0300

    implement core methods

--- src/main/kotlin/data/repositories/ProjectsRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/ProjectsRepositoryImpl.kt
@@ -2,18 +2,37 @@ package data.repositories
 
 import logic.models.Project
 import logic.repositoies.ProjectsRepository
-import java.util.UUID
+import java.util.*
 
-class ProjectsRepositoryImpl: ProjectsRepository {
-    override fun getProjectById(projectId: UUID): Project {
-        TODO("Not yet implemented")
+class ProjectsRepositoryImpl : ProjectsRepository {
+    private val projects = mutableMapOf<UUID, Project>()
+    private val projectUserAssignments = mutableMapOf<UUID, MutableSet<UUID>>()
+
+    override fun save(project: Project): Project {
+        projects[project.id] = project
+        return project
     }
 
-    override fun saveProject(project: Project) {
-        TODO("Not yet implemented")
+    override fun findById(id: UUID): Project? {
+        return projects[id]
     }
 
-    override fun deleteProject(projectId: UUID) {
-        TODO("Not yet implemented")
+    override fun delete(id: UUID): Boolean {
+        return if (projects.containsKey(id)) {
+            projects.remove(id)
+            projectUserAssignments.remove(id)
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun assignUserToProject(projectId: UUID, userId: UUID): Boolean {
+        if (!projects.containsKey(projectId)) {
+            return false
+        }
+
+        val userSet = projectUserAssignments.getOrPut(projectId) { mutableSetOf() }
+        return userSet.add(userId)
     }
 }
