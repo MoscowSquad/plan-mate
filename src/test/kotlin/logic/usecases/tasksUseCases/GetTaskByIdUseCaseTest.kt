70627e32dc60accc65242a8a278bf113ff033638
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-01 15:50:25 +0300

    separate get task by ID test into GetTaskByIdUseCaseTest class

---
+++ src/test/kotlin/logic/usecases/tasksUseCases/GetTaskByIdUseCaseTest.kt
@@ -0,0 +1,68 @@
+package logic.usecases.tasksUseCases
+
+import io.mockk.every
+import io.mockk.mockk
+import logic.models.Task
+import logic.repositoies.TasksRepository
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class GetTaskByIdUseCaseTest {
+    private lateinit var getTaskByIdUseCase: GetTaskByIdUseCase
+    private lateinit var tasksRepository: TasksRepository
+
+    @BeforeEach
+    fun setup() {
+        tasksRepository = mockk(relaxed = true)
+        getTaskByIdUseCase = GetTaskByIdUseCase(tasksRepository)
+    }
+
+    @Test
+    fun `should return task model when found task successfully `() {
+        // Given
+        val tasks: List<Task> = listOf(
+            Task(id = 1234, title = "Videos1"),
+            Task(id = 12345, title = "Videos2")
+        )
+        val input = 1234
+        every { tasksRepository.getAll() } returns tasks
+
+        val expected = Task(id = 1234, title = "Videos1")
+
+        // When
+        val result = getTaskByIdUseCase.getTaskById(input)
+
+        // Then
+        assertEquals(result, expected)
+    }
+
+    @Test
+    fun `should Throw exception when wanted task not found`() {
+        // Given
+        val tasks: List<Task> = listOf(
+            Task(id = 1234, title = "Videos1"),
+            Task(id = 12345, title = "Videos2")
+        )
+        every { tasksRepository.getAll() } returns tasks
+        val input = 456
+
+        // When & Then
+        assertThrows<Exception> {
+            getTaskByIdUseCase.getTaskById(input)
+        }
+    }
+
+    @Test
+    fun `should Throw exception when there is no tasks`() {
+        // Given
+        every { tasksRepository.getAll() } returns emptyList()
+        val input = 456
+
+        // When & Then
+        assertThrows<Exception> {
+            getTaskByIdUseCase.getTaskById(input)
+        }
+    }
+}
