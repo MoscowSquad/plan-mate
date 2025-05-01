ac8616ed4c22287f2a802dc83414405aa2925847
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-01 17:50:08 +0300

    refactor Exception to meaningful Exception class and reach coverage to 100%

--- src/test/kotlin/logic/usecases/tasksUseCases/ChangeTaskStateUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/tasksUseCases/ChangeTaskStateUseCaseTest.kt
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import utilities.PropertyNullException
+import utilities.TaskIsNotFoundException
 import java.util.*
 
 class ChangeTaskStateUseCaseTest {
@@ -43,7 +45,7 @@ class ChangeTaskStateUseCaseTest {
     }
 
     @Test
-    fun `should Throw exception when task ID is null during change state `() {
+    fun `should Throw PropertyNullException when task ID is null during change state `() {
         // Given
         val id: UUID = mockk()
         val projectId: UUID = mockk()
@@ -55,54 +57,55 @@ class ChangeTaskStateUseCaseTest {
         every { tasksRepository.getAll() } returns tasks
         val input = Task(id = null, title = "Videos1", state = State(id, "TODO", projectId))
         // When & Then
-        assertThrows<Exception> {
+        assertThrows<PropertyNullException> {
             changeTaskStateUseCase.changeTaskState(input)
         }
     }
 
     @Test
-    fun `should Throw exception when wanted task is not found`() {
+    fun `should Throw TaskIsNotFoundException when wanted task is not found`() {
         // Given
         val id: UUID = mockk()
         val projectId: UUID = mockk()
         val tasks: List<Task> = listOf(
-            Task(id = 1234, title = "Videos1", state = State(id, "TODO", projectId)),
+            Task(id = null, title = "Videos1", state = State(id, "TODO", projectId)),
             Task(id = 12345, title = "Videos2", state = State(id, "TODO", projectId))
         )
         every { tasksRepository.getAll() } returns tasks
         val input = Task(id = 123, title = "Videos1", state = State(id, "TODO", projectId))
         // When & Then
-        assertThrows<Exception> {
+        assertThrows<TaskIsNotFoundException> {
             changeTaskStateUseCase.changeTaskState(input)
         }
     }
 
     @Test
-    fun `should Throw exception when there is no tasks found to change state`() {
+    fun `should Throw TaskIsNotFoundException when there is no tasks found to change state`() {
         // Given
         val id: UUID = mockk()
         val projectId: UUID = mockk()
         every { tasksRepository.getAll() } returns emptyList()
         val input = Task(id = 123, title = "Videos1", state = State(id, "TODO", projectId))
         // When & Then
-        assertThrows<Exception> {
+        assertThrows<TaskIsNotFoundException> {
             changeTaskStateUseCase.changeTaskState(input)
         }
     }
 
     @Test
-    fun `should Throw exception when taskToEdit has null ID`() {
+    fun `should Throw PropertyNullException when taskToEdit has null ID`() {
         // Given
         val id: UUID = mockk()
         val projectId: UUID = mockk()
         val tasks: List<Task> = listOf(
-            Task(id = null, title = "Videos1", state = State(id, "TODO", projectId))
+            Task(id = 123, title = "Videos1", state = State(id, "TODO", projectId))
         )
         every { tasksRepository.getAll() } returns tasks
 
         // When & Then
-        assertThrows<Exception> {
-            changeTaskStateUseCase.changeTaskState(Task(id = 123, state = State(id, "Done", projectId)))
+        assertThrows<PropertyNullException> {
+            changeTaskStateUseCase.changeTaskState(Task(id = null, state = State(id, "Done", projectId)))
         }
     }
+
 }
