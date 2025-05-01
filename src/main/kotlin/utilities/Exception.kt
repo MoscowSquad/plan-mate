3f2d601745712f2cd39b466a530aa6ecc20a14a1
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-01 16:39:51 +0300

    add to Exceptions classes TaskIsNotFoundException && PropertyNullException

--- src/main/kotlin/utilities/Exception.kt
+++ src/main/kotlin/utilities/Exception.kt
@@ -4,4 +4,6 @@ import java.util.UUID
 
 
 class NoExistProjectException(projectId: UUID) : Exception("Project '$projectId' does not exist")
+class TaskIsNotFoundException(taskId:Int) : Exception("Task '$taskId' does not exist")
+class PropertyNullException() : Exception("your input has null value")
 
