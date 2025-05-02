77f418a9ab220e9392e1c113b1023e11a3c642a2
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-02 10:21:29 +0300

    refactor code in DeleteTaskUseCase

--- src/main/kotlin/logic/usecases/tasksUseCases/DeleteTaskUseCase.kt
+++ src/main/kotlin/logic/usecases/tasksUseCases/DeleteTaskUseCase.kt
@@ -4,12 +4,13 @@ import logic.models.Task
 import logic.repositoies.TasksRepository
 import utilities.PropertyNullException
 import utilities.TaskIsNotFoundException
+import java.util.UUID
 
 class DeleteTaskUseCase(
     private val tasksRepository: TasksRepository
 ) {
 
-    fun deleteTask(id: Int?): List<Task> {
+    fun deleteTask(id: UUID?): List<Task> {
         val allTasks = tasksRepository.getAll().toMutableList()
         val taskId = id?:throw  PropertyNullException()
         if (allTasks.isEmpty()) {
