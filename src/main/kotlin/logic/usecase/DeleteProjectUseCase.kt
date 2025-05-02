ed2b4d280286f42d46573eb6add0c719c733b8fb
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-05-02 16:27:55 +0300

    test: create test cases for DeleteProjectUseCase

---
+++ src/main/kotlin/logic/usecase/DeleteProjectUseCase.kt
@@ -0,0 +1,24 @@
+package logic.usecase
+
+import logic.repositoies.ProjectsRepository
+import utilities.NoExistProjectException
+import utilities.NotAdminException
+import java.util.UUID
+
+class DeleteProjectUseCase(
+    private val projectsRepository: ProjectsRepository
+) {
+
+    fun invoke(id: UUID, isAdmin: Boolean): Boolean {
+        if (!isAdmin) {
+            throw NotAdminException("Only administrators can delete projects")
+        }
+
+        val success = projectsRepository.delete(id)
+        if (!success) {
+            throw NoExistProjectException(id)
+        }
+
+        return true
+    }
+}
