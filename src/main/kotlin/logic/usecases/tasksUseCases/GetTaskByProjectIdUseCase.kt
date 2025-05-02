685edad4c8ef47bc460dfd813a649778ae389697
=Muhammed Wael <=mw26200400@gmail.com>
2025-05-02 11:47:49 +0300

    refactor GetTaskByProjectIdUseCase

--- src/main/kotlin/logic/usecases/tasksUseCases/GetTaskByProjectIdUseCase.kt
+++ src/main/kotlin/logic/usecases/tasksUseCases/GetTaskByProjectIdUseCase.kt
@@ -2,17 +2,14 @@ package logic.usecases.tasksUseCases
 
 import logic.models.Task
 import logic.repositoies.TasksRepository
-import utilities.PropertyNullException
-import utilities.TaskIsNotFoundException
 import java.util.*
 
 class GetTaskByProjectIdUseCase(
     private val tasksRepository: TasksRepository
 ) {
-    fun getTaskByProjectId(id: UUID?): Task {
-        val allTasks = tasksRepository.getAll()
-        val projectID = id ?: throw PropertyNullException()
-
-        return allTasks.find { it.projectId == id } ?: throw TaskIsNotFoundException(projectID)
+    fun getTaskByProjectId(id: UUID): List<Task> {
+        return tasksRepository.getAll().filter { currentTask ->
+            currentTask.projectId == id
+        }
     }
 }
