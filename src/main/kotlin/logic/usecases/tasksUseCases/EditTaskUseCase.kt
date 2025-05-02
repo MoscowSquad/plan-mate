46311f206e62699e9bddbef5512a1d7b1b742e86
=Muhammed Wael <=mw26200400@gmail.com>
2025-05-02 11:47:00 +0300

    refactor EditTaskUseCase

--- src/main/kotlin/logic/usecases/tasksUseCases/EditTaskUseCase.kt
+++ src/main/kotlin/logic/usecases/tasksUseCases/EditTaskUseCase.kt
@@ -2,23 +2,26 @@ package logic.usecases.tasksUseCases
 
 import logic.models.Task
 import logic.repositoies.TasksRepository
-import utilities.PropertyNullException
 import utilities.TaskIsNotFoundException
 
 class EditTaskUseCase(
     private val tasksRepository: TasksRepository
 ) {
-    fun editTask(task: Task): Task {
+    fun editTask(task: Task): Boolean {
         val allTasks = tasksRepository.getAll().toMutableList()
-        val taskID = task.id?: throw PropertyNullException()
-        val taskTitle = task.title ?: throw PropertyNullException()
-        val newTask = allTasks
-            .find { currentTask -> currentTask.id == taskID } ?: throw TaskIsNotFoundException(taskID)
+        val taskIndex: Int = allTasks.indexOfFirst { it.id == task.id }
 
-        newTask.title = taskTitle
-        newTask.stateId = task.stateId
-        newTask.description = task.description
+        if (taskIndex == -1) {
+            throw TaskIsNotFoundException(task.id)
+        }
 
-        return newTask
+        allTasks[taskIndex] = task.copy(
+            id = task.id,
+            title = task.title,
+            description = task.description,
+            stateId = task.stateId
+        )
+
+        return true
     }
 }
