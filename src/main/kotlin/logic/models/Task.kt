253e35d89828cf39396f2e1f1cad9be604246fe7
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-02 10:22:14 +0300

    refactor code in Task model

--- src/main/kotlin/logic/models/Task.kt
+++ src/main/kotlin/logic/models/Task.kt
@@ -3,9 +3,9 @@ package logic.models
 import java.util.*
 
 data class Task(
-    val id: UUID,
-    val title: String,
-    val description: String,
+    val id: UUID?,
+    var title: String?,
+    var description: String="",
     val projectId: UUID,
-    val stateId: UUID
+    var stateId: UUID?,
 )
