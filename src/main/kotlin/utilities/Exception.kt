be4634a58b82f30dea636475efdb4ef64e6a8253
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-05-02 14:43:30 +0300

    add exception for unauthorized access

--- src/main/kotlin/utilities/Exception.kt
+++ src/main/kotlin/utilities/Exception.kt
@@ -4,3 +4,4 @@ import java.util.UUID
 
 class NoExistProjectException(projectId: UUID) : Exception("Project '$projectId' does not exist")
 
+class UnauthorizedAccessException(message: String) : Exception(message)
