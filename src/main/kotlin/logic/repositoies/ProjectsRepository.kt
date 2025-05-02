26dbe2a56d50e5cebfa20c52446e1dcd83db2f06
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-05-02 14:39:35 +0300

    add core method to ProjectsRepository.kt

--- src/main/kotlin/logic/repositoies/ProjectsRepository.kt
+++ src/main/kotlin/logic/repositoies/ProjectsRepository.kt
@@ -1,5 +1,12 @@
 package logic.repositoies
 
+import logic.models.Project
+import java.util.UUID
+
 interface ProjectsRepository {
+    fun add(project: Project): Boolean
+    fun update(project: Project): Boolean
+    fun delete(id: UUID): Boolean
+    fun getAll(): List<Project>
 
 }
