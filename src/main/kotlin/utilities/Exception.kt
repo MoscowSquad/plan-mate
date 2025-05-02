a5676c5e170a695b02c4aac291731ca3f83c6c68
Yasser Ahmed <yasserahmed.fci@gmail.com>
2025-05-02 16:01:48 +0300

    add exception

--- src/main/kotlin/utilities/Exception.kt
+++ src/main/kotlin/utilities/Exception.kt
@@ -3,4 +3,8 @@ package utilities
 import java.util.UUID
 
 class NoExistProjectException(projectId: UUID) : Exception("Project '$projectId' does not exist")
+class NotAdminException(message: String) : Exception(message)
+class InvalidProjectNameException(message: String) : Exception(message)
+class ProjectCreationFailedException(message: String) : Exception(message)
+class ProjectNotFoundException(id: UUID) : NoSuchElementException("Project with id $id not found")
 
