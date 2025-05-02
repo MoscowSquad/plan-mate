f1321940b16665913583e8cd928c9d2406f6cee4
Aseel AbuKmail <aseelabukmail@gmail.com>
2025-05-02 20:18:42 +0300

    add implementation for audit-repository

--- src/main/kotlin/data/repositories/AuditRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/AuditRepositoryImpl.kt
@@ -1,21 +1,62 @@
 package data.repositories
 
+import kotlinx.datetime.LocalDateTime
 import logic.models.AuditLog
+import logic.models.AuditType
 import logic.repositoies.AuditRepository
 import java.io.File
 import java.util.*
 
 class AuditRepositoryImpl(private val csvFile: File) : AuditRepository {
 
+    init {
+        if (!csvFile.exists()) {
+            csvFile.createNewFile()
+            csvFile.appendText("id,auditType,action,timestamp,entityId,userId\n")
+        }
+    }
+
     override fun add(log: AuditLog?): Boolean {
-        TODO("Not yet implemented")
+        try {
+            csvFile.appendText(
+                "${log?.id},${log?.auditType},${log?.action}," +
+                        "${log?.timestamp},${log?.entityId},${log?.userId}\n"
+            )
+            return true
+        } catch (e: Exception){
+            return false
+        }
     }
 
     override fun getAllByTaskId(taskId: UUID): List<AuditLog> {
-        TODO("Not yet implemented")
-    }
+        return getAllLogs().filter { it.auditType == AuditType.TASK && it.entityId == taskId }    }
 
     override fun getAllByProjectId(projectId: UUID): List<AuditLog> {
-        TODO("Not yet implemented")
+        return getAllLogs().filter { it.auditType == AuditType.PROJECT && it.entityId == projectId }
+    }
+
+    //
+    private fun getAllLogs(): List<AuditLog> {
+        return csvFile.readLines()
+            .drop(1)
+            .mapNotNull { line ->
+                val parts = line.split(',')
+                if (parts.size == 6) {
+                    try {
+                        AuditLog(
+                            id = UUID.fromString(parts[0]),
+                            auditType = AuditType.valueOf(parts[1]),
+                            action = parts[2],
+                            timestamp = LocalDateTime.parse(parts[3]),
+                            entityId = UUID.fromString(parts[4]),
+                            userId = UUID.fromString(parts[5])
+                        )
+                    } catch (e: Exception) {
+                        null
+                    }
+                } else {
+                    null
+                }
+            }
     }
 }
