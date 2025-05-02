a7b9f34075b64dbf941dfe3ad02c66479f5e8163
Hasan Ali <ihasanalihasan@gmail.com>
2025-05-02 21:07:47 +0300

    add AddStateUseCase with implementation

--- src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
+++ src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
@@ -2,17 +2,20 @@ package logic.usecases.state
 
 import logic.models.TaskState
 import logic.repositoies.StateRepository
+import utilities.IllegalStateTitle
 
 class AddStateUseCase(
     private val stateRepository: StateRepository,
 ) {
 
-    operator fun invoke(state: TaskState): Boolean{
-        isValidTitleState(state.title)
-        return false// fake
+    operator fun invoke(state: TaskState): Boolean {
+        val isValid = isValidTitleState(state.title)
+        return if (isValid) stateRepository.add(state.projectId, state = state) else {
+            throw IllegalStateTitle("Title Task State is not valid")
+        }
     }
 
-    private fun isValidTitleState(title: String): Boolean{
-        return false
+    private fun isValidTitleState(title: String): Boolean {
+        return !(title.isNotBlank() && title.isNotEmpty())
     }
 }
