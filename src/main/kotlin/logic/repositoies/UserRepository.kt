7afc5ede92dfa7e5894376e784d78e9315c38667
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-05-02 12:16:31 +0300

    add operations

---
+++ src/main/kotlin/logic/repositoies/UserRepository.kt
@@ -0,0 +1,13 @@
+package logic.repositoies
+
+import logic.models.User
+import java.util.*
+
+interface UserRepository {
+    fun add(user: User): Boolean
+    fun delete(id: UUID): Boolean
+    fun assignToProject(projectId: UUID, userId: UUID): Boolean
+    fun revokeFromProject(projectId: UUID, userId: UUID): Boolean
+    fun getById(id: UUID): User 
+    fun getAll(): List<User>
+}
