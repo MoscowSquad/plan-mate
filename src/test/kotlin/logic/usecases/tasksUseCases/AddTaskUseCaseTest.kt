aa002d7c6de30db0ecc14a1ea598a4ae3ebdabde
=Muhammed Wael <=mw26200400@gmail.com>
2025-05-02 11:46:21 +0300

    refactor AddTaskUseCaseTest

--- src/test/kotlin/logic/usecases/tasksUseCases/AddTaskUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/tasksUseCases/AddTaskUseCaseTest.kt
@@ -4,11 +4,9 @@ import io.mockk.every
 import io.mockk.mockk
 import logic.models.Task
 import logic.repositoies.TasksRepository
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import utilities.PropertyNullException
 import java.util.*
 
 class AddTaskUseCaseTest {
@@ -27,17 +25,21 @@ class AddTaskUseCaseTest {
     fun `should return list of Tasks when add task without any issue`() {
         // Given
         val tasks: List<Task> = listOf(
-            Task(id=id, title = "Videos",projectId=id,stateId = id),
-            Task(id=id2, title = "Videos2",projectId=id2,stateId = id2),
+            Task(id = id, title = "Videos", description = "description", projectId = id, stateId = id),
+            Task(id = id2, title = "Videos2", description = "description", projectId = id2, stateId = id2),
             )
-        val task = Task(id = UUID.fromString("00000000-0000-0000-0000-000000000003")
-            , title = "Videos3"  ,projectId=id2,stateId = id2)
+        val task = Task(id = UUID.fromString("00000000-0000-0000-0000-000000000003"),
+            title = "Videos3",
+            projectId = id2,
+            description = "description",
+            stateId = id2
+        )
 
         every { tasksRepository.getAll() } returns tasks
 
         val expected = listOf(
-            Task(id=id, title = "Videos",projectId=id,stateId = id),
-            Task(id=id2, title = "Videos2",projectId=id2,stateId = id2),
+            Task(id = id, title = "Videos", projectId = id, description = "description", stateId = id),
+            Task(id = id2, title = "Videos2", projectId = id2, description = "description", stateId = id2),
             task,
         )
 
@@ -46,51 +48,4 @@ class AddTaskUseCaseTest {
         // Then
         assertEquals(result, expected)
     }
-    @Test
-    fun `should Throw PropertyNullException when  ID is null `() {
-        // Given
-        val tasks: List<Task> = listOf(
-            Task(id=null, title = "Videos",projectId=id,stateId = id),
-            Task(id=id2, title = "Videos2",projectId=id2,stateId = id2),
-        )
-        every { tasksRepository.getAll() } returns tasks
-
-        // When & Then
-        assertThrows<PropertyNullException> {
-            addTaskUseCase.addTask(Task(id = null
-                , title = "Videos3"  ,projectId= UUID.fromString("00000000-0000-0000-0000-000000000003"),stateId = UUID.fromString("00000000-0000-0000-0000-000000000003")))
-        }
-    }
-    @Test
-    fun `should Throw PropertyNullException when user title is null`() {
-        // Given
-        val tasks: List<Task> = listOf(
-            Task(id=id, title = "Videos",projectId=id,stateId = id),
-            Task(id=id2, title = "Videos2",projectId=id2,stateId = id2),
-        )
-        every { tasksRepository.getAll() } returns tasks
-
-        // When & Then
-        assertThrows<PropertyNullException> {
-            addTaskUseCase.addTask(Task(id = UUID.fromString("00000000-0000-0000-0000-000000000003")
-                , title = null  ,projectId=id2,stateId = id2))
-        }
-    }
-
-    @Test
-    fun `should Throw PropertyNullException when state ID is null `() {
-        // Given
-        val tasks: List<Task> = listOf(
-            Task(id=id, title = "Videos",projectId=id,stateId = null),
-            Task(id=id2, title = "Videos2",projectId=id2,stateId = id2),
-        )
-        every { tasksRepository.getAll() } returns tasks
-
-        // When & Then
-        assertThrows<PropertyNullException> {
-            addTaskUseCase.addTask(Task(id = UUID.fromString("00000000-0000-0000-0000-000000000003")
-                , title = "Videos3"  ,projectId= UUID.fromString("00000000-0000-0000-0000-000000000003"),stateId = null))
-        }
-    }
-
 }
