c2a55517927ec87d636beb7343dc3f1281804915
=Muhammed Wael <=mw26200400@gmail.com>
2025-05-02 11:39:35 +0300

    refactor addTask

--- src/main/kotlin/logic/usecases/tasksUseCases/AddTaskUseCase.kt
+++ src/main/kotlin/logic/usecases/tasksUseCases/AddTaskUseCase.kt
@@ -2,18 +2,12 @@ package logic.usecases.tasksUseCases
 
 import logic.models.Task
 import logic.repositoies.TasksRepository
-import utilities.PropertyNullException
 
 class AddTaskUseCase(
     private val tasksRepository: TasksRepository
 ) {
-    fun addTask(task: Task): List<Task> {
+    fun addTask(task: Task): Boolean {
         val allTasks = tasksRepository.getAll().toMutableList()
-        if (task.title == null || task.id == null || task.stateId == null) {
-            throw PropertyNullException()
-        }
-        allTasks.add(task)
-        return allTasks
+        return allTasks.add(task)
     }
-
 }
