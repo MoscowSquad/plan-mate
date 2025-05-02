8be33f41c4cdcc090f42521f13f014e4654203cf
=Muhammed Wael <=mw26200400@gmail.com>
2025-05-02 17:28:46 +0300

    add TasksRepository methods

--- src/main/kotlin/logic/repositoies/TasksRepository.kt
+++ src/main/kotlin/logic/repositoies/TasksRepository.kt
@@ -1,7 +1,13 @@
 package logic.repositoies
 
 import logic.models.Task
+import java.util.*
 
 interface TasksRepository {
     fun getAll(): List<Task>
+    fun addTask(task: Task): Boolean
+    fun editTask(updatedTask: Task): Boolean
+    fun deleteTask(taskId: UUID): Boolean
+    fun getTaskById(taskId: UUID): Task
+    fun getTaskByProjectId(taskId: UUID): List<Task>
 }
