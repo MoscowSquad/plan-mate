91fcba3eaa4a50e54aeda119d3ad2c37c7b64112
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-05-02 16:10:49 +0300

    delete unnecessary exception

--- src/main/kotlin/utilities/Exception.kt
+++ src/main/kotlin/utilities/Exception.kt
@@ -1,7 +1,3 @@
 package utilities
 
-import java.util.UUID
-
-class NoExistProjectException(projectId: UUID) : Exception("Project '$projectId' does not exist")
-
 class UnauthorizedAccessException(message: String) : Exception(message)
