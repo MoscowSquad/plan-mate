2c00b8626f8b0d8e3b25148965c927793fcd6f35
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-03 11:52:15 +0300

    refactor GetByIdUseCaseTest

--- src/test/kotlin/logic/usecases/task/GetByIdUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/task/GetByIdUseCaseTest.kt
@@ -2,6 +2,7 @@ package logic.usecases.task
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import logic.models.Task
 import logic.repositories.TasksRepository
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -10,11 +11,11 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import utilities.TaskIsNotFoundException
 import java.util.*
+import kotlin.test.assertTrue
 
 class GetByIdUseCaseTest {
     private lateinit var getTaskByIdUseCase: GetTaskByIdUseCase
     private lateinit var tasksRepository: TasksRepository
-    val id = UUID.fromString("00000000-0000-0000-0000-000000000001")
     val id2 = UUID.fromString("00000000-0000-0000-0000-000000000002")
 
     @BeforeEach
@@ -25,48 +26,26 @@ class GetByIdUseCaseTest {
 
     @Test
     fun `should return task model when found task successfully `() {
-        // Given
-        val tasks: List<Task> = listOf(
-            Task(id = id, title = "Videos", projectId = id, description = "description", stateId = id),
-            Task(id = id2, title = "Videos2", projectId = id2, description = "description", stateId = id2),
-        )
-        val input = id
-        every { tasksRepository.getAll() } returns tasks
 
-        val expected = Task(id = id, title = "Videos", projectId = id, description = "description", stateId = id)
+        // Given
+        val id = UUID.fromString("00000000-0000-0000-0000-000000000001")
+
+        val task = Task(
+            id = id,
+            title = "Videos",
+            projectId = UUID.randomUUID(),
+            description = "description",
+            stateId = UUID.randomUUID()
+        )
+
+        every { tasksRepository.getById(id) } returns task
+
 
         // When
-        val result = getTaskByIdUseCase(input)
+        val result = getTaskByIdUseCase(id)
 
         // Then
-        assertEquals(result, expected)
-    }
-
-    @Test
-    fun `should Throw TaskIsNotFoundException when wanted task not found`() {
-        // Given
-        val tasks: List<Task> = listOf(
-            Task(id = id, title = "Videos", projectId = id, description = "description", stateId = id),
-            Task(id = id2, title = "Videos2", projectId = id2, description = "description", stateId = id2),
-        )
-        every { tasksRepository.getAll() } returns tasks
-        val input =  UUID.fromString("00000000-0000-0000-0000-000000000003")
-
-        // When & Then
-        assertThrows<TaskIsNotFoundException> {
-            getTaskByIdUseCase(input)
-        }
-    }
-
-    @Test
-    fun `should Throw TaskIsNotFoundException when there is no tasks`() {
-        // Given
-        every { tasksRepository.getAll() } returns emptyList()
-        val input =  UUID.fromString("00000000-0000-0000-0000-000000000002")
-
-        // When & Then
-        assertThrows<TaskIsNotFoundException> {
-            getTaskByIdUseCase(input)
-        }
+        assertEquals(result,task)
+        verify(exactly = 1) { getTaskByIdUseCase(id) }
     }
 }
