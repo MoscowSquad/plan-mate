91ca23a56ee9d6c767007af27ea7b4f27444258d
Hasan Ali <ihasanalihasan@gmail.com>
2025-05-03 13:38:27 +0300

    add implementation of AddStateUseCase and add test cases then archive 100 test coverage and all test cases passed

--- src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
+++ src/main/kotlin/logic/usecases/state/AddStateUseCase.kt
@@ -2,17 +2,24 @@ package logic.usecases.state
 
 import logic.models.TaskState
 import logic.repositories.StateRepository
+import utilities.IllegalStateTitle
 
 class AddStateUseCase(
     private val stateRepository: StateRepository,
 ) {
 
-    operator fun invoke(state: TaskState): Boolean{
-        isValidTitleState(state.title)
-        return false// fake
+    operator fun invoke(state: TaskState): Boolean {
+        require(isValidTitle(state.title)) {
+            throw IllegalStateTitle("Task state title cannot be blank")
+        }
+
+        return stateRepository.add(state.projectId, state)
+            .also { success ->
+                if (!success) throw IllegalStateException("Failed to add state")
+            }
     }
 
-    private fun isValidTitleState(title: String): Boolean{
-        return false
+    private fun isValidTitle(title: String): Boolean {
+        return title.isNotBlank() && title.length in 1..100
     }
 }
