fecc7d954363b77e10613db91dc068b85ae82140
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-08 11:26:46 +0300

    implement Task state data source

--- src/main/kotlin/data/mongodb_data/datasource/TaskStateDataSourceImpl.kt
+++ src/main/kotlin/data/mongodb_data/datasource/TaskStateDataSourceImpl.kt
@@ -1,27 +1,41 @@
 package data.mongodb_data.datasource
 
+import com.mongodb.client.model.Filters
+import com.mongodb.kotlin.client.coroutine.MongoCollection
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.toList
+import logic.models.Project
 import logic.models.TaskState
 import java.util.*
 
-class TaskStateDataSourceImpl():TaskStateDataSource {
+class TaskStateDataSourceImpl(
+    private val collection: MongoCollection<TaskState>
+
+):TaskStateDataSource {
     override suspend fun getTaskStateById(id: UUID): TaskState {
-        TODO("Not yet implemented")
+        val filter = Filters.eq("id", id.toString())
+        return collection.find(filter).firstOrNull()
+            ?: throw NoSuchElementException("Task state not found")
     }
 
     override suspend fun getTaskStateByProjectId(projectId: UUID): List<TaskState> {
-        TODO("Not yet implemented")
+        val filter = Filters.eq("entityId", projectId.toString())
+        return collection.find(filter).toList()
     }
 
-    override suspend fun updateTaskState(state: TaskState): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun updateTaskState(state: TaskState){
+        val filter = Filters.eq("id", state.id)
+        collection.replaceOne(filter, state)
     }
 
-    override suspend fun addTaskState(projectId: UUID, state: TaskState): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun addTaskState(projectId: UUID, state: TaskState){
+        collection.insertOne(state)
     }
 
-    override suspend fun deleteTaskState(projectId: UUID, stateId: UUID): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun deleteTaskState(projectId: UUID, stateId: UUID){
+        val filter = Filters.eq("id", projectId.toString())
+        collection.deleteOne(filter)
     }
 
 }
