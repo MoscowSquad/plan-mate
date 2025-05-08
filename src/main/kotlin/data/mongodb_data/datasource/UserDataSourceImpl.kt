bd1439cab57e6e6e3ef3005d8f9e834fda390e22
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-08 13:24:59 +0300

    refactor UserDataSourceImpl code

--- src/main/kotlin/data/mongodb_data/datasource/UserDataSourceImpl.kt
+++ src/main/kotlin/data/mongodb_data/datasource/UserDataSourceImpl.kt
@@ -5,6 +5,8 @@ import com.mongodb.kotlin.client.coroutine.MongoCollection
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.toList
 import logic.models.User
+import logic.util.ProjectNotFoundException
+import logic.util.UserNotFoundException
 import java.util.*
 
 class UserDataSourceImpl(private val collection: MongoCollection<User>):UserDataSource {
@@ -16,41 +18,37 @@ class UserDataSourceImpl(private val collection: MongoCollection<User>):UserData
 
     override suspend fun deleteUser(id: UUID): Boolean {
         val filter = Filters.eq("id", id.toString())
-        collection.deleteOne(filter)
-        return true
+        return collection.deleteOne(filter).wasAcknowledged()
     }
 
     override suspend fun assignUserToProject(projectId: UUID, userId: UUID): Boolean {
         val filter = Filters.eq("id", userId)
         val user = collection.find(filter).firstOrNull() ?: return false
         val updatedProjectIds = user.projectIds.toMutableList().apply { add(projectId) }
-
         val updatedUser = user.copy(projectIds = updatedProjectIds)
 
-        val updateResult = collection.replaceOne(filter, updatedUser)
-        return updateResult.modifiedCount > 0
+        return collection.replaceOne(filter, updatedUser).modifiedCount > 0
     }
 
     override suspend fun unassignUserFromProject(projectId: UUID, userId: UUID): Boolean {
         val filter = Filters.eq("id", userId)
-        val user = collection.find(filter).firstOrNull() ?: return false
+        val user = collection.find(filter).firstOrNull() ?: throw UserNotFoundException("User is not found")
 
         if (!user.projectIds.contains(projectId)) {
-            return false
+            throw ProjectNotFoundException(projectId)
         }
 
         val updatedProjectIds = user.projectIds.filterNot { it == projectId }
-
         val updatedUser = user.copy(projectIds = updatedProjectIds)
 
-        val updateResult = collection.replaceOne(filter, updatedUser)
-        return updateResult.modifiedCount > 0
+        return collection.replaceOne(filter, updatedUser).modifiedCount > 0
+
     }
 
     override suspend fun getUserById(id: UUID): User {
         val filter = Filters.eq("id", id.toString())
         return collection.find(filter).firstOrNull()
-            ?: throw NoSuchElementException("User is not found")
+            ?: throw throw UserNotFoundException("User is not found")
     }
 
     override suspend fun getAllUsers(): List<User> {
