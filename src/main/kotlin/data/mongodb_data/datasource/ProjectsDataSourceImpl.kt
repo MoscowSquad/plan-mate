c35dcac5687ede601e6678eab8a8fb2fa6d99aa8
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-08 13:24:24 +0300

    refactor ProjectsDataSourceImpl code

--- src/main/kotlin/data/mongodb_data/datasource/ProjectsDataSourceImpl.kt
+++ src/main/kotlin/data/mongodb_data/datasource/ProjectsDataSourceImpl.kt
@@ -3,8 +3,10 @@ package data.mongodb_data.datasource
 import com.mongodb.client.model.Filters
 import com.mongodb.kotlin.client.coroutine.MongoCollection
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.toList
 import logic.models.Project
+import logic.util.ProjectNotFoundException
 import java.util.*
 
 class ProjectsDataSourceImpl(
@@ -12,21 +14,18 @@ class ProjectsDataSourceImpl(
 
 ): ProjectsDataSource{
     override suspend fun addProject(project: Project):Boolean {
-        val addedProject = collection.insertOne(project)
-        return addedProject.wasAcknowledged()
+        return collection.insertOne(project).wasAcknowledged()
     }
 
 
     override suspend fun updateProject(project: Project):Boolean {
         val filter = Filters.eq("id", project.id)
-        val updatedProject =collection.replaceOne(filter, project)
-        return updatedProject.modifiedCount>0
+        return collection.replaceOne(filter, project).modifiedCount>0
     }
 
     override suspend fun deleteProject(id: UUID):Boolean {
         val filter = Filters.eq("id", id.toString())
-        val deletedProject =collection.deleteOne(filter)
-        return deletedProject.deletedCount>0
+        return collection.deleteOne(filter).deletedCount>0
     }
 
     override suspend fun getAllProjects(): List<Project> {
@@ -35,7 +34,7 @@ class ProjectsDataSourceImpl(
 
     override suspend fun getProjectById(id: UUID): Project {
         val filter = Filters.eq("entityId", id.toString())
-        return collection.find(filter).first()
+        return collection.find(filter).firstOrNull()?:throw ProjectNotFoundException(id)
     }
 
 }
