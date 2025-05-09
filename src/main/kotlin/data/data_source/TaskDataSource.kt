2846160767c8d533b09e223281a3c35b30b02ab3
=Muhammed Wael <=mw26200400@gmail.com>
2025-05-09 17:49:42 +0300

    update TaskDataSource interface to return Boolean

--- src/main/kotlin/data/data_source/TaskDataSource.kt
+++ src/main/kotlin/data/data_source/TaskDataSource.kt
@@ -1,13 +1,13 @@
 package data.data_source
 
 import data.mongodb_data.dto.TaskDto
-import java.util.UUID
+import java.util.*
 
 interface TaskDataSource {
     suspend fun getAllTasks(): List<TaskDto>
-    suspend fun addTask(task: TaskDto)
-    suspend fun editTask(updatedTask: TaskDto)
-    suspend fun deleteTask(taskId: UUID)
+    suspend fun addTask(task: TaskDto): Boolean
+    suspend fun editTask(updatedTask: TaskDto): Boolean
+    suspend fun deleteTask(taskId: UUID): Boolean
     suspend fun getTaskById(taskId: UUID): TaskDto
     suspend fun getTaskByProjectId(projectId: UUID): List<TaskDto>
 
