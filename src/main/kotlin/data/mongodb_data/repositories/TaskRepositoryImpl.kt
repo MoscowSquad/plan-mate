2df479f245c1863a6d60f21a6725cf07c2339297
=Muhammed Wael <=mw26200400@gmail.com>
2025-05-09 17:52:32 +0300

    refactor TaskRepositoryImpl

--- src/main/kotlin/data/mongodb_data/repositories/TaskRepositoryImpl.kt
+++ src/main/kotlin/data/mongodb_data/repositories/TaskRepositoryImpl.kt
@@ -5,7 +5,6 @@ import data.data_source.TaskDataSource
 import data.mongodb_data.mappers.toDto
 import data.mongodb_data.mappers.toTask
 import data.mongodb_data.util.executeInIO
-import kotlinx.coroutines.runBlocking
 import logic.models.Task
 import logic.repositories.TasksRepository
 import java.util.*
@@ -22,41 +21,19 @@ class TaskRepositoryImpl(
         }
     }
 
-    override fun addTask(task: Task): Boolean {
-        return executeInIO {
-            try {
-                taskDataSource.addTask(task.toDto())
-                true
-            } catch (e: Exception) {
-                false
-            }
-        }
+    override fun addTask(task: Task) = executeInIO {
+        taskDataSource.addTask(task.toDto())
     }
 
 
-    override fun editTask(updatedTask: Task): Boolean {
-        return runBlocking {
-            try {
-                taskDataSource.editTask(updatedTask.toDto())
-                true
-            } catch (e: Exception) {
-                false
-            }
-        }
+    override fun editTask(updatedTask: Task) = executeInIO {
+        taskDataSource.editTask(updatedTask.toDto())
     }
 
 
-    override fun deleteTask(taskId: UUID): Boolean {
-        return executeInIO {
-            try {
-                taskDataSource.deleteTask(taskId)
-                true
-            } catch (e: Exception) {
-                false
-            }
-        }
+    override fun deleteTask(taskId: UUID) = executeInIO {
+        taskDataSource.deleteTask(taskId)
     }
-
 
     override fun getTaskById(taskId: UUID) = executeInIO {
         taskDataSource.getTaskById(taskId).toTask()
