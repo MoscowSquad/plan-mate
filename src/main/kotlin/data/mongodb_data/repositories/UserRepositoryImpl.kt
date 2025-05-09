b3814722bf2de9b74e645da18c4220d5449c8134
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-05-09 17:21:27 +0300

    refactor UserRepositoryImpl

--- src/main/kotlin/data/mongodb_data/repositories/UserRepositoryImpl.kt
+++ src/main/kotlin/data/mongodb_data/repositories/UserRepositoryImpl.kt
@@ -3,51 +3,39 @@ package data.mongodb_data.repositories
 import data.csv_data.mappers.toDto
 import data.csv_data.mappers.toUser
 import data.data_source.UserDataSource
+import data.mongodb_data.mappers.toTaskState
+import data.mongodb_data.util.executeInIO
 import kotlinx.coroutines.*
 import logic.models.User
 import logic.repositories.UserRepository
 import java.util.*
 
-class UserRepositoryImpl (private val userDataSource: UserDataSource): UserRepository {
+class UserRepositoryImpl(private val userDataSource: UserDataSource) : UserRepository {
     private val scope = CoroutineScope(Dispatchers.IO)
 
-    override fun addUser(user: User): Boolean {
-        val deferred = scope.async {
-            userDataSource.addUser(user.toDto())
+    override fun addUser(user: User): Boolean =
+        executeInIO { userDataSource.addUser(user.toDto()) }
+
+
+    override fun deleteUser(id: UUID): Boolean =
+        executeInIO { userDataSource.deleteUser(id) }
+
+
+    override fun assignUserToProject(projectId: UUID, userId: UUID): Boolean =
+        executeInIO { userDataSource.assignUserToProject(projectId, userId) }
+
+    override fun unassignUserFromProject(projectId: UUID, userId: UUID): Boolean =
+        executeInIO { userDataSource.unassignUserFromProject(projectId, userId) }
+
+    override fun getUserById(id: UUID): User =
+        executeInIO { userDataSource.getUserById(id).toUser() }
+
+
+    override fun getAllUsers(): List<User> =
+        executeInIO {
+            userDataSource.getAllUsers().toList().map {
+                it.toUser()
+            }
         }
-        return deferred.getCompleted()
-    }
-
-    override fun deleteUser(id: UUID): Boolean {
-        val deferred = scope.async {
-            userDataSource.deleteUser(id) }
-        return deferred.getCompleted()
-    }
-
-    override fun assignUserToProject(projectId: UUID, userId: UUID): Boolean {
-        val deferred = scope.async {
-            userDataSource.assignUserToProject(projectId,userId) }
-        return deferred.getCompleted()
-    }
-
-    override fun unassignUserFromProject(projectId: UUID, userId: UUID): Boolean {
-        val deferred = scope.async {
-            userDataSource.unassignUserFromProject(projectId,userId) }
-        return deferred.getCompleted()
-    }
-
-    override fun getUserById(id: UUID): User {
-        val deferred = scope.async {
-            userDataSource.getUserById(id) }
-
-        return deferred.getCompleted().toUser()
-    }
-
-    override fun getAllUsers(): List<User> {
-        val deferred = scope.async {
-            userDataSource.getAllUsers().toList() }
-        return deferred.getCompleted().map { it.toUser()
-        }
-    }
 
 }
