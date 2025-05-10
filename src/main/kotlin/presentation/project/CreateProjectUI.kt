3388e647037af8f4c25afd32cb62376526c9ad65
Hasan Ali <ihasanalihasan@gmail.com>
2025-05-10 15:15:00 +0300

    set default task state when create project

--- src/main/kotlin/presentation/project/CreateProjectUI.kt
+++ src/main/kotlin/presentation/project/CreateProjectUI.kt
@@ -1,29 +1,53 @@
 package presentation.project
 
+import logic.models.TaskState
 import logic.models.UserRole
 import logic.usecases.project.CreateProjectUseCase
+import logic.usecases.task_state.AddTaskStateUseCase
 import presentation.io.ConsoleIO
 import presentation.session.SessionManager
+import java.util.*
 
 class CreateProjectUI(
     private val createProjectUseCase: CreateProjectUseCase,
+    private val addTaskStateUseCase: AddTaskStateUseCase,
     private val consoleIO: ConsoleIO
 ) : ConsoleIO by consoleIO {
     operator fun invoke() {
         write("Enter project name:")
         val projectName = read()
 
+
         runCatching {
-            createProjectUseCase(
+            val projectId = createProjectUseCase(
                 name = projectName,
                 isAdmin = SessionManager.currentUser?.role == UserRole.ADMIN
             )
+            val defaultStates = listOf(
+                TaskState(
+                    id = UUID.randomUUID(),
+                    name = "Todo",
+                    projectId = projectId
+                ),
+                TaskState(
+                    id = UUID.randomUUID(),
+                    name = "In Progress",
+                    projectId = projectId
+                ),
+                TaskState(
+                    id = UUID.randomUUID(),
+                    name = "Done",
+                    projectId = projectId
+                )
+            )
+
+            defaultStates.forEach { addTaskStateUseCase(it) }
         }
             .onSuccess {
-                write("Project $it created successfully!")
+                write("✅Project created successfully!")
             }
             .onFailure {
-                write("Error creating project: ${it.message}")
+                write("❌Error creating project: ${it.message}")
             }
     }
 }
