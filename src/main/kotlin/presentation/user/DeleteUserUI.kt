a17f889758b1761f1e4f5262b9960814d1f55e94
alaafawzy <alaaf9443@gmail.com>
2025-05-13 16:39:35 +0300

    remove  try catch to handle craches in usecase

--- src/main/kotlin/presentation/user/DeleteUserUI.kt
+++ src/main/kotlin/presentation/user/DeleteUserUI.kt
@@ -4,7 +4,6 @@ import data.mongodb_data.mappers.toUUID
 import di.SessionManager
 import logic.models.UserRole
 import logic.usecases.user.DeleteUserUseCase
-import logic.util.UnauthorizedAccessException
 import presentation.io.ConsoleIO
 
 class DeleteUserUI(
@@ -13,10 +12,6 @@ class DeleteUserUI(
 ) : ConsoleIO by consoleIO {
     operator fun invoke() {
         val currentUserRole = SessionManager.getCurrentUserRole()
-        if (currentUserRole != UserRole.ADMIN) {
-            write("\nError: Only ADMIN users can delete accounts.")
-            return
-        }
 
         write("\n=======================")
         write("║      DELETE USER     ║")
@@ -27,19 +22,20 @@ class DeleteUserUI(
         val userId = try {
             input.toUUID()
         } catch (e: IllegalArgumentException) {
-            write("Error: Invalid UUID format. Please provide a valid user ID.")
+            write("❌ Error: Invalid UUID format. Please provide a valid user ID.")
             return
         }
 
-        try {
+        val result = runCatching {
             deleteUserUseCase(currentUserRole, userId)
-            write("User with ID $userId has been successfully deleted.")
-        } catch (e: UnauthorizedAccessException) {
-            write("Error: You don't have permission to delete users.")
-        } catch (e: NoSuchElementException) {
-            write("Error: User with ID $userId not found.")
-        } catch (e: Exception) {
-            write("Error: An unexpected error occurred while deleting the user.")
         }
+
+        result
+            .onSuccess {
+                write("✅ User with ID $userId has been successfully deleted.")
+            }
+            .onFailure {
+                write("❌ Failed to delete user. ${it.message}")
+            }
     }
 }
