b2f9766290df43d560083aad4e5718c2a740df98
AimanYosofi <=>
2025-05-15 20:37:05 +0300

    fix the name of the repository object inside the Audit use-cases

--- src/main/kotlin/logic/usecases/audit/ViewAuditLogsByProjectUseCase.kt
+++ src/main/kotlin/logic/usecases/audit/ViewAuditLogsByProjectUseCase.kt
@@ -4,8 +4,8 @@ import logic.models.AuditLog
 import logic.repositories.AuditRepository
 import java.util.UUID
 
-class ViewAuditLogsByProjectUseCase(private val auditLogUseCase: AuditRepository) {
-    operator fun invoke(projectId: UUID): List<AuditLog>{
-        return auditLogUseCase.getAllLogsByProjectId(projectId)
+class ViewAuditLogsByProjectUseCase(private val auditRepository: AuditRepository) {
+    operator fun invoke(projectId: UUID): List<AuditLog> {
+        return auditRepository.getAllLogsByProjectId(projectId)
     }
 }
--- src/main/kotlin/logic/usecases/audit/ViewAuditLogsByTaskUseCase.kt
+++ src/main/kotlin/logic/usecases/audit/ViewAuditLogsByTaskUseCase.kt
@@ -4,8 +4,8 @@ import logic.models.AuditLog
 import logic.repositories.AuditRepository
 import java.util.UUID
 
-class ViewAuditLogsByTaskUseCase(private val auditLogUseCase: AuditRepository) {
-    operator fun invoke(taskId: UUID):  List<AuditLog>{
-        return auditLogUseCase.getAllLogsByTaskId(taskId)
+class ViewAuditLogsByTaskUseCase(private val auditRepository: AuditRepository) {
+    operator fun invoke(taskId: UUID): List<AuditLog> {
+        return auditRepository.getAllLogsByTaskId(taskId)
     }
 }
