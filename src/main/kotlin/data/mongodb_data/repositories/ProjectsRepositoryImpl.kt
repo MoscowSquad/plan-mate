4a1f361429b0b0940a254daf031032f01ec2fac0
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-05-16 17:33:16 +0300

    fix ProjectsRepositoryImpl to integrate user assignment and enhance project retrieval logic

--- src/main/kotlin/data/mongodb_data/repositories/ProjectsRepositoryImpl.kt
+++ src/main/kotlin/data/mongodb_data/repositories/ProjectsRepositoryImpl.kt
@@ -3,6 +3,7 @@ package data.mongodb_data.repositories
 
 import data.data_source.AuditLogDataSource
 import data.data_source.ProjectsDataSource
+import data.data_source.UserDataSource
 import data.mongodb_data.dto.AuditLogDto
 import data.mongodb_data.mappers.toDto
 import data.mongodb_data.mappers.toProject
@@ -11,6 +12,8 @@ import data.session_manager.SessionManager
 import domain.models.AuditLog.AuditType
 import domain.models.Project
 import domain.repositories.ProjectsRepository
+import domain.util.NoExistProjectException
+import domain.util.NoUserLoginException
 import kotlinx.datetime.Clock
 import java.util.*
 
@@ -18,10 +21,18 @@ import java.util.*
 class ProjectsRepositoryImpl(
     private val projectsDataSource: ProjectsDataSource,
     private val auditLogDataSource: AuditLogDataSource,
+    private val userDataSource: UserDataSource
 ) : ProjectsRepository {
 
     override suspend fun addProject(project: Project) = executeInIO {
         val result = projectsDataSource.addProject(project.toDto())
+        userDataSource.assignUserToProject(
+            project.id,
+            SessionManager.currentUser?.id ?: throw NoUserLoginException()
+        )
+        SessionManager.currentUser = SessionManager.currentUser?.copy(
+            projectIds = SessionManager.currentUser?.projectIds?.plus(project.id) ?: listOf(project.id)
+        )
         auditLogDataSource.addLog(
             log = AuditLogDto(
                 id = UUID.randomUUID().toString(),
@@ -63,7 +74,7 @@ class ProjectsRepositoryImpl(
     }
 
     override suspend fun getAllProjectsByUser(userId: UUID) = executeInIO {
-        val userProjects = SessionManager.currentUser!!.projectIds
+        val userProjects = SessionManager.currentUser?.projectIds ?: throw NoExistProjectException(userId)
         projectsDataSource.getAllProjects()
             .map { it.toProject() }
             .filter { project -> userProjects.contains(project.id) }
--- src/main/kotlin/di/mongodbRepositoryModule.kt
+++ src/main/kotlin/di/mongodbRepositoryModule.kt
@@ -7,7 +7,7 @@ import org.koin.dsl.module
 val mongodbRepositoryModule = module {
     single<UserRepository> { UserRepositoryImpl(get(), get()) }
     single<TasksRepository> { TaskRepositoryImpl(get(), get()) }
-    single<ProjectsRepository> { ProjectsRepositoryImpl(get(), get()) }
+    single<ProjectsRepository> { ProjectsRepositoryImpl(get(), get(), get()) }
     single<AuditRepository> { AuditLogRepositoryImpl(get()) }
     single<TaskStateRepository> { TaskStateRepositoryImpl(get(), get()) }
     single<AuthenticationRepository> { UserRepositoryImpl(get(), get()) }
