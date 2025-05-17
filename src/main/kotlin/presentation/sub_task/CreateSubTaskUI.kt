7d5747f9a4337a9fd02c3c4bcc51bde8067341e8
alaafawzy <alaaf9443@gmail.com>
2025-05-17 20:25:31 +0300

    add create sub task ui

--- src/main/kotlin/presentation/sub_task/CreateSubTaskUI.kt
+++ src/main/kotlin/presentation/sub_task/CreateSubTaskUI.kt
@@ -1,28 +1,40 @@
 package presentation.sub_task
 
+import domain.models.SubTask
 import domain.usecases.sub_task.CreateSubTaskUseCase
+import presentation.io.ConsoleIO
+import java.util.UUID
 
 class CreateSubTaskUI(
-    private val createSubTaskUseCase: CreateSubTaskUseCase
-) {/*
+    private val createSubTaskUseCase: CreateSubTaskUseCase,
+    private val consoleIO: ConsoleIO
+
+) : ConsoleIO by consoleIO{
+
     suspend operator fun invoke() {
         write("Please enter task ID:")
         val taskId = readUUID()
-        write("Please enter task name:")
-        val subTaskName = readUUID()
-        write("Please enter task ID:")
-        val taskId = readUUID()
+        write("Please enter sup task title:")
+        val subTaskTitle = read()
+        write("Please enter sub task description:")
+        val subTaskDescription= read()
 
 
         if (taskId == null) {
             write("❌ Invalid task ID.")
             return
         }
-
-        runCatching { deleteTaskUseCase(taskId) }
-            .onSuccess { write("✅ Task deleted successfully.") }
-            .onFailure { write("❌ Failed to delete task: ${it.message}") }
+        val subTask= SubTask(
+            title = subTaskTitle,
+            isCompleted = false,
+            description = subTaskDescription,
+            parentTaskId = taskId,
+            id = UUID.randomUUID()
+        )
+        runCatching { createSubTaskUseCase(subTask) }
+            .onSuccess { write("✅ sub task added successfully.") }
+            .onFailure { write("❌ failed to add sub  task: ${it.message}") }
 
     }
-    */
+
 }
