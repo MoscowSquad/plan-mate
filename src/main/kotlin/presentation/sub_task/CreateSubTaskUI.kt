3289616b0ab0a5ed79965bd4ae3d262fc2770047
alaafawzy <alaaf9443@gmail.com>
2025-05-17 18:10:29 +0300

    add create percentage for all subTask ui class

--- src/main/kotlin/presentation/sub_task/CreateSubTaskUI.kt
+++ src/main/kotlin/presentation/sub_task/CreateSubTaskUI.kt
@@ -4,8 +4,25 @@ import domain.usecases.sub_task.CreateSubTaskUseCase
 
 class CreateSubTaskUI(
     private val createSubTaskUseCase: CreateSubTaskUseCase
-) {
+) {/*
     suspend operator fun invoke() {
+        write("Please enter task ID:")
+        val taskId = readUUID()
+        write("Please enter task name:")
+        val subTaskName = readUUID()
+        write("Please enter task ID:")
+        val taskId = readUUID()
+
+
+        if (taskId == null) {
+            write("❌ Invalid task ID.")
+            return
+        }
+
+        runCatching { deleteTaskUseCase(taskId) }
+            .onSuccess { write("✅ Task deleted successfully.") }
+            .onFailure { write("❌ Failed to delete task: ${it.message}") }
 
     }
+    */
 }
