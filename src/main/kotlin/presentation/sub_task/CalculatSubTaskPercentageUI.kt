5da9d4b0e4b949143fa2a5f4874b4e06f89fc34a
alaafawzy <alaaf9443@gmail.com>
2025-05-17 20:25:50 +0300

    add calculate percentage for sub task ui

--- src/main/kotlin/presentation/sub_task/CalculatSubTaskPercentageUI.kt
+++ src/main/kotlin/presentation/sub_task/CalculatSubTaskPercentageUI.kt
@@ -1,4 +1,30 @@
 package presentation.sub_task
 
-class CalculatSubTaskPercentageUI {
+import domain.usecases.sub_task.GetSubTaskPrcentageUseCase
+import presentation.io.ConsoleIO
+
+
+class CalculatSubTaskPercentageUI(
+    private val getSubTaskPercentageUseCase: GetSubTaskPrcentageUseCase,
+    private val consoleIO: ConsoleIO
+) : ConsoleIO by consoleIO {
+
+    suspend operator fun invoke() {
+        /*
+        write("Please enter Task ID:")
+        val taskId = readUUID()
+
+        if (taskId == null) {
+            write("❌ Invalid Task ID.")
+            return
+        }
+*/
+        runCatching { getSubTaskPercentageUseCase() }
+            .onSuccess { percentage ->
+                write("✅ Sub-task completion percentage: %.2f%%".format(percentage))
+            }
+            .onFailure {
+                write("❌ Failed to calculate percentage: ${it.message}")
+            }
+    }
 }
