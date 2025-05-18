ffda432f17e221c2544204593ef12cd6e6cfaf0e
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-05-18 06:42:17 +0300

    delete unnecessary sub task percentage use case

--- src/main/kotlin/domain/usecases/sub_task/GetSubTaskPercentageUseCase.kt
+++
@@ -1,9 +0,0 @@
-package domain.usecases.sub_task
-
-import domain.models.Task
-
-class GetSubTaskPrcentageUseCase(private val subTask: Task) {
-    operator fun invoke(): Double {
-        return subTask.calculateSubTaskCompletionPercentage()
-    }
-}
